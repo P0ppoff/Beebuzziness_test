faf5a1b1ed6f6f8d901224432a03d02c071942ef
Jules H <jules.hablot@gmail.com>
2019-08-31 11:49:35 +0200

    TDD: add digits test

--- test/digits.spec.js
+++ test/digits.spec.js
@@ -8,4 +8,25 @@ describe('Constants', () => {
       ['.', '_', '.'],
     ]);
   });
+  it('should return digit 1 model', () => {
+    expect(DIGITS.DIGIT1).toEqual([
+      ['.', '.', '.'],
+      ['.', '.', '|'],
+      ['.', '.', '|'],
+    ]);
+  });
+  it('should return digit 2 model', () => {
+    expect(DIGITS.DIGIT2).toEqual([
+      ['.', '_', '.'],
+      ['.', '_', '|'],
+      ['|', '_', '.'],
+    ]);
+  });
+  it('should return digit 3 model', () => {
+    expect(DIGITS.DIGIT3).toEqual([
+      ['.', '_', '.'],
+      ['.', '_', '|'],
+      ['.', '_', '|'],
+    ]);
+  });
 });
