361476f4ac06f62c0be484b25ac015daac3caaf8
Jules H <jules.hablot@gmail.com>
2019-08-31 11:51:44 +0200

    add digits to pass test

--- src/digits.enum.js
+++ src/digits.enum.js
@@ -8,6 +8,27 @@ const DIGIT0 = [
   [DOT, UNDERSCORE, DOT],
 ];
 
+const DIGIT1 = [
+  [DOT, DOT, DOT],
+  [DOT, DOT, PIPE],
+  [DOT, DOT, PIPE],
+];
+
+const DIGIT2 = [
+  [DOT, UNDERSCORE, DOT],
+  [DOT, UNDERSCORE, PIPE],
+  [PIPE, UNDERSCORE, DOT],
+];
+
+const DIGIT3 = [
+  [DOT, UNDERSCORE, DOT],
+  [DOT, UNDERSCORE, PIPE],
+  [DOT, UNDERSCORE, PIPE],
+];
+
 module.exports = {
   DIGIT0,
+  DIGIT1,
+  DIGIT2,
+  DIGIT3,
 };
