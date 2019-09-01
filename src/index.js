b5ac00621f47c547102b1061c1fbd67a19468706
Jules H <jules.hablot@gmail.com>
2019-09-01 19:09:48 +0200

    simple managment of user input

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
+const argv = require('minimist')(process.argv.slice(2));
 const { printDigit } = require('./printDigit');
 
-if (process.argv.length === 3) {
-  printDigit(process.argv[2]);
+const printUsage = () => {
+  console.log(`
+  Ce programme s’utilise avec un nombre en base 10 comme paramètre. Il sera ensuite converti en 9 segments.
+  `);
+};
+
+const givenNumber = argv._[0];
+
+if (!argv.h && !Number.isNaN(Number(givenNumber))) {
+  console.log();
+  printDigit(`${givenNumber}`);
+  console.log();
+} else {
+  printUsage();
 }
